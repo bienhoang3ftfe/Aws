2715d5b9027cd5737c1e0c974a48f4039db60d50
1thoihoangkim <120029631+1thoihoangkim@users.noreply.github.com>
2023-09-05 09:41:09 +0700

    s

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello....git add README.md')
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+https://runkit.com/
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
